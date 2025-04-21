34c8f5d79910fe771647f89368fa5448b0922274
kdiggety <wakes-pillory.01@icloud.com>
2025-04-20 20:50:52 -0700

    adjusting header formatting

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ function Header({ userProfile }) {
     <div className="app-header">
       <h1>Hobby Investor</h1>
       <div className="div-right">
-        <p className="bold">{userProfile.name}</p>
+        <div>
+          <span className="bold">Investor: </span>
+          <span>{userProfile.name}</span>
+        </div>
         <div>
           <span className="bold">Account Number: </span>
           <span>{userProfile.accountNumber}</span>
